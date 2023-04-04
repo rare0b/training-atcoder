0834c4654c584dbc3680551f2abfc7c19515a4cc
rare0b <rare0b0110@gmail.com>
2023-04-05 01:51:03 +0900

    Solved 1AC on 2023-04-04

---
+++ Daily/20230404.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+// https://atcoder.jp/contests/practice2/tasks/practice2_i
+
+int main() {
+    string s;
+    cin >> s;
+    vector<int> sa = suffix_array(s);
+    for (auto x : sa) {
+        cout << x << " ";
+    }
+    long long ans = s.size() * (s.size() + 1) / 2;
+    for (auto x : lcp_array(s, sa)) {
+        ans -= x;
+        cout << x << " ";
+    }
+    cout << endl;
+    cout << ans << endl;
+}
