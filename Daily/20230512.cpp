75121dbf39db727237c969e28fb55786b800c690
rare0b <rare0b0110@gmail.com>
2023-05-12 20:20:02 +0900

    Solved 1AC on 2023-05-12

--- Daily/20230512.cpp
+++ Daily/20230512.cpp
@@ -14,13 +14,28 @@ int main() {
     int x;
     cin >> x;
     vector<int> a(N);
-    for (int i = 0; i < N; i++) cin >> a[i];
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    if (sum == x) {
+        cout << N << endl;
+        return 0;
+    } else if (sum < x) {
+        cout << N - 1 << endl;
+        return 0;
+    }
+
     sort(a.begin(), a.end());
     int ans = 0;
+
     for (int i = 0; i < N; i++) {
         if (x - a[i] >= 0) {
             x -= a[i];
             ans++;
+        } else {
+            break;
         }
     }
 
